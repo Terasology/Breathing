376c76f18b871b35f7deb6926744294a7c0f7368
DarkWeird <foxflameless@gmail.com>
2020-08-20 20:58:12 +0300

    fix(NUI): fix after NUI extraction

--- src/main/java/org/terasology/breathing/ui/BreathingBar.java
+++ src/main/java/org/terasology/breathing/ui/BreathingBar.java
@@ -1,18 +1,5 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.breathing.ui;
 
 import org.slf4j.Logger;
@@ -24,9 +11,9 @@ import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.logic.players.LocalPlayer;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.registry.In;
-import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
+import org.terasology.nui.databinding.ReadOnlyBinding;
 import org.terasology.rendering.nui.layers.hud.CoreHudWidget;
-import org.terasology.rendering.nui.widgets.UIIconBar;
+import org.terasology.nui.widgets.UIIconBar;
 
 public class BreathingBar extends CoreHudWidget {
 
