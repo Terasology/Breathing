fa3e08371eb559236283a82c636e527d622de798
DarkWeird <foxflameless@gmail.com>
2020-09-09 17:06:52 +0300

    feature(reintegrate-gestaltv7):  gestalt-v7 compability (package)

--- src/main/java/org/terasology/breathing/BreathingSystem.java
+++ src/main/java/org/terasology/breathing/BreathingSystem.java
@@ -1,44 +1,31 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2020 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.breathing;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.breathing.component.BreatherComponent;
 import org.terasology.breathing.component.SuffocatingComponent;
-import org.terasology.engine.Time;
-import org.terasology.entitySystem.entity.EntityManager;
-import org.terasology.entitySystem.entity.EntityRef;
-import org.terasology.entitySystem.event.ReceiveEvent;
-import org.terasology.entitySystem.systems.BaseComponentSystem;
-import org.terasology.entitySystem.systems.RegisterMode;
-import org.terasology.entitySystem.systems.RegisterSystem;
-import org.terasology.entitySystem.systems.UpdateSubscriberSystem;
-import org.terasology.logic.characters.CharacterMovementComponent;
-import org.terasology.logic.characters.events.OnEnterBlockEvent;
-import org.terasology.logic.health.EngineDamageTypes;
-import org.terasology.logic.health.event.DoDamageEvent;
-import org.terasology.logic.location.LocationComponent;
+import org.terasology.engine.core.Time;
+import org.terasology.engine.entitySystem.entity.EntityManager;
+import org.terasology.engine.entitySystem.entity.EntityRef;
+import org.terasology.engine.entitySystem.event.ReceiveEvent;
+import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
+import org.terasology.engine.entitySystem.systems.RegisterMode;
+import org.terasology.engine.entitySystem.systems.RegisterSystem;
+import org.terasology.engine.entitySystem.systems.UpdateSubscriberSystem;
+import org.terasology.engine.logic.characters.CharacterMovementComponent;
+import org.terasology.engine.logic.characters.events.OnEnterBlockEvent;
+import org.terasology.engine.logic.destruction.EngineDamageTypes;
+import org.terasology.engine.logic.location.LocationComponent;
+import org.terasology.engine.registry.CoreRegistry;
+import org.terasology.engine.registry.In;
+import org.terasology.engine.world.BlockEntityRegistry;
+import org.terasology.engine.world.WorldProvider;
+import org.terasology.engine.world.block.Block;
+import org.terasology.engine.world.block.BlockManager;
+import org.terasology.health.logic.event.DoDamageEvent;
 import org.terasology.math.geom.Vector3i;
-import org.terasology.registry.CoreRegistry;
-import org.terasology.registry.In;
-import org.terasology.world.BlockEntityRegistry;
-import org.terasology.world.WorldProvider;
-import org.terasology.world.block.Block;
-import org.terasology.world.block.BlockManager;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
 public class BreathingSystem extends BaseComponentSystem implements UpdateSubscriberSystem {
@@ -58,7 +45,8 @@ public class BreathingSystem extends BaseComponentSystem implements UpdateSubscr
 
     @Override
     public void update(float delta) {
-        for (EntityRef entity : entityManager.getEntitiesWith(SuffocatingComponent.class, BreatherComponent.class, LocationComponent.class)) {
+        for (EntityRef entity : entityManager.getEntitiesWith(SuffocatingComponent.class, BreatherComponent.class,
+                LocationComponent.class)) {
             SuffocatingComponent suffocating = entity.getComponent(SuffocatingComponent.class);
             LocationComponent loc = entity.getComponent(LocationComponent.class);
             BreatherComponent breather = entity.getComponent(BreatherComponent.class);
@@ -72,7 +60,8 @@ public class BreathingSystem extends BaseComponentSystem implements UpdateSubscr
                 if (gameTime > suffocating.nextSuffocateDamageTime) {
                     // damage the entity
                     EntityRef liquidBlock = blockEntityProvider.getBlockEntityAt(loc.getWorldPosition());
-                    entity.send(new DoDamageEvent(breather.suffocateDamage, EngineDamageTypes.DROWNING.get(), liquidBlock));
+                    entity.send(new DoDamageEvent(breather.suffocateDamage, EngineDamageTypes.DROWNING.get(),
+                            liquidBlock));
                     // set the next damage time
                     suffocating.nextSuffocateDamageTime = gameTime + breather.timeBetweenSuffocateDamage;
                     entity.saveComponent(suffocating);
@@ -121,7 +110,8 @@ public class BreathingSystem extends BaseComponentSystem implements UpdateSubscr
             String breathableMaterial = breather.breathes.get(i);
 
             if (breathableMaterial.equalsIgnoreCase("Oxygen") && block.equals(blockManager.getBlock(BlockManager.AIR_ID))
-                    || breathableMaterial.equalsIgnoreCase("Water") && block.equals(blockManager.getBlock("CoreAssets:Water"))) {
+                    || breathableMaterial.equalsIgnoreCase("Water") && block.equals(blockManager.getBlock("CoreAssets" +
+                    ":Water"))) {
 
                 return true;
             }
--- src/main/java/org/terasology/breathing/package-info.java
+++ src/main/java/org/terasology/breathing/package-info.java
@@ -1,2 +1,4 @@
-/** Contains classes for breathing / suffocation of arbitrary entities in various media
- *  Additionally includes a UI component for displaying the player's breath */
+/**
+ * Contains classes for breathing / suffocation of arbitrary entities in various media Additionally includes a UI
+ * component for displaying the player's breath
+ */
