e29861df2e2f2aa37e8bb06496cc3aa10f668e46
Michael Pollind <polli104@mail.chapman.edu>
2020-12-13 05:48:03 -0800

    feat(JOML): migrate charactermovment (#13)

--- src/main/java/org/terasology/breathing/BreathingSystem.java
+++ src/main/java/org/terasology/breathing/BreathingSystem.java
@@ -15,6 +15,7 @@
  */
 package org.terasology.breathing;
 
+import org.joml.Vector3i;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.terasology.breathing.component.BreatherComponent;
@@ -32,7 +33,6 @@ import org.terasology.logic.characters.events.OnEnterBlockEvent;
 import org.terasology.logic.health.EngineDamageTypes;
 import org.terasology.logic.health.event.DoDamageEvent;
 import org.terasology.logic.location.LocationComponent;
-import org.terasology.math.geom.Vector3i;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.registry.In;
 import org.terasology.world.BlockEntityRegistry;
@@ -121,7 +121,7 @@ public class BreathingSystem extends BaseComponentSystem implements UpdateSubscr
             String breathableMaterial = breather.breathes.get(i);
 
             if (breathableMaterial.equalsIgnoreCase("Oxygen") && block.equals(blockManager.getBlock(BlockManager.AIR_ID))
-                    || breathableMaterial.equalsIgnoreCase("Water") && block.equals(blockManager.getBlock("CoreAssets:Water"))) {
+                || breathableMaterial.equalsIgnoreCase("Water") && block.equals(blockManager.getBlock("CoreAssets:Water"))) {
 
                 return true;
             }
