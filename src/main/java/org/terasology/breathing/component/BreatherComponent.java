d0783ec7ff963c298135c1d5a87e3b1a1f4a8a12
Nail Khanipov <foxflameless@gmail.com>
2021-08-25 16:24:22 +0300

    fix(ecs-gestalt): Migrate Components to gestalt's Components. (#18)
    Ref: MovingBlocks/Terasology#4753
    
    Co-authored-by: Tobias Nett <skaldarnar@googlemail.com>

--- src/main/java/org/terasology/breathing/component/BreatherComponent.java
+++ src/main/java/org/terasology/breathing/component/BreatherComponent.java
@@ -1,21 +1,9 @@
-/*
- * Copyright 2015 MovingBlocks
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 package org.terasology.breathing.component;
 
-import org.terasology.engine.entitySystem.Component;
+import com.google.common.collect.Lists;
+import org.terasology.gestalt.entitysystem.component.Component;
 
 import java.util.Arrays;
 import java.util.List;
@@ -23,7 +11,7 @@ import java.util.List;
 /**
  * Use to signify an entity is subject to org.terasology.breathing
  */
-public class BreatherComponent implements Component {
+public class BreatherComponent implements Component<BreatherComponent> {
 
     public long breathCapacity = 15000;
     public float breathRechargeRate = 2.0f;
@@ -32,4 +20,12 @@ public class BreatherComponent implements Component {
 
     public List<String> breathes = Arrays.asList("Oxygen");
 
+    @Override
+    public void copyFrom(BreatherComponent other) {
+        this.breathCapacity = other.breathCapacity;
+        this.breathRechargeRate = other.breathRechargeRate;
+        this.timeBetweenSuffocateDamage = other.timeBetweenSuffocateDamage;
+        this.suffocateDamage = other.suffocateDamage;
+        this.breathes = Lists.newArrayList(other.breathes);
+    }
 }
