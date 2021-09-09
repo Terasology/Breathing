f5f1b170fa72502f41e72c30d101caa6e87aff56
darkweird <foxflameless@gmail.com>
2021-09-09 11:18:57 +0300

    feature(gestaltv7-events): replace ReceiveEvent with gestaltv7's ReceiveEvent

--- src/main/java/org/terasology/breathing/BreathingSystem.java
+++ src/main/java/org/terasology/breathing/BreathingSystem.java
@@ -24,7 +24,6 @@ import org.terasology.breathing.component.SuffocatingComponent;
 import org.terasology.engine.core.Time;
 import org.terasology.engine.entitySystem.entity.EntityManager;
 import org.terasology.engine.entitySystem.entity.EntityRef;
-import org.terasology.engine.entitySystem.event.ReceiveEvent;
 import org.terasology.engine.entitySystem.systems.BaseComponentSystem;
 import org.terasology.engine.entitySystem.systems.RegisterMode;
 import org.terasology.engine.entitySystem.systems.RegisterSystem;
@@ -39,6 +38,7 @@ import org.terasology.engine.world.BlockEntityRegistry;
 import org.terasology.engine.world.WorldProvider;
 import org.terasology.engine.world.block.Block;
 import org.terasology.engine.world.block.BlockManager;
+import org.terasology.gestalt.entitysystem.event.ReceiveEvent;
 import org.terasology.module.health.events.DoDamageEvent;
 
 @RegisterSystem(RegisterMode.AUTHORITY)
