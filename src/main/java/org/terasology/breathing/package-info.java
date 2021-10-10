fa05b085649c4a0ed04526a56fa00c921d5bc058
mohamedation <mohamedation@gmail.com>
2021-10-10 21:16:11 +0200

    updated Copyright

--- src/main/java/org/terasology/breathing/package-info.java
+++ src/main/java/org/terasology/breathing/package-info.java
@@ -1,3 +1,5 @@
+// Copyright 2021 The Terasology Foundation
+// SPDX-License-Identifier: Apache-2.0
 /** Contains classes for breathing / suffocation of arbitrary entities in various media
  *  Additionally includes a UI component for displaying the player's breath */
 package org.terasology.breathing;
